406e83d9239f14a81f20a9936145021d9f53434a
ManuelDiLena <manuel.dilena.29@gmail.com>
2024-03-13 20:39:14 +0100

    RowEmpty component

--- src/components/Wordle.tsx
+++ src/components/Wordle.tsx
@@ -1,9 +1,11 @@
 import RowCompleted from "./RowCompleted";
+import RowEmpty from "./RowEmpty";
 
 export default function Wordle() {
   return (
     <div>
       <RowCompleted word='abolish' solution='billion' />
+      <RowEmpty />
     </div>
   );
 }
