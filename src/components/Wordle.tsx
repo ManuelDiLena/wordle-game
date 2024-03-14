1d4fda5fdaff6d7d9e15b00e760e853baf157fd2
ManuelDiLena <manuel.dilena.29@gmail.com>
2024-03-14 20:49:27 +0100

    Writing and erasing word

--- src/components/Wordle.tsx
+++ src/components/Wordle.tsx
@@ -13,18 +13,58 @@ export default function Wordle() {
   const [completedWords, setCompletedWords] = useState<string[]>([]);
   const [gameStatus, setGameStatus] = useState<GameStatus>(GameStatus.Playing);
 
+  const keys = [
+    'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P',
+    'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L',
+    'Z', 'X', 'C', 'V', 'B', 'N', 'M'
+  ]
+
   useWindow('keydown', handleKeyDown);
 
   useEffect(() => {
     setWordOfTheDay('airport')
   })
 
-  function handleKeyDown(event: KeyboardEvent) {}
+  // Function to validate keyboard events
+  function handleKeyDown(event: KeyboardEvent) {
+    const letter = event.key.toUpperCase();
+
+    if (event.key === 'Backspace' && currentWord.length > 0) {
+      onDelete()
+      return;
+    }
+
+    if (event.key === 'Enter') {
+      return;
+    }
+
+    if (currentWord.length >= 7) {
+      return;
+    }
+
+    // Enter the letter of the state
+    if (keys.includes(letter)) {
+      onInput(letter)
+      return;
+    }
+  }
+
+  // Function to add the letter to the box
+  function onInput(letter: string) {
+    const newWord = currentWord + letter
+    setCurrentWord(newWord)
+  }
+
+  // Function to delete the letter from the box
+  function onDelete() {
+    const newWord = currentWord.slice(0, -1)
+    setCurrentWord(newWord)
+  }
 
   return (
     <div>
       <RowCompleted word='abolish' solution={wordOfTheDay} />
-      <RowCurrent word='abo' />
+      <RowCurrent word={currentWord} />
       <RowEmpty />
       <RowEmpty />
       <RowEmpty />
